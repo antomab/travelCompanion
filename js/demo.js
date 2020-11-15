94f00bbde4cda203f457424f54f2a7a7078b9f04
antonia <antonia.mab@gmail.com>
2020-11-15 18:49:29 +0000

    touch events complete and carousel
    capture all relevant touch events
    carousel for menu set up
    added some audios and assets

--- js/demo.js
+++ js/demo.js
@@ -1,82 +1,90 @@
 function demo () {
     var EVENTS = {
-        tap: 'tap',
+        singleTap: 'tap',
         doubleTap: 'doubletap',
+        twoFingerTap: 'taptwofinger',
         press: 'press',
         pinch: 'pinch',
-        pinchIn: 'pinchin',
+        pinchEnd: 'pinchend',
         pinchOut: 'pinchout',
-        swipe: 'swipe',
-        swipeUp: 'swipeup',
-        swipeDown: 'swipeDown'
-    }
+        pinchIn: 'pinchin'
+    };
     var scenario1 = $('#scenario1')[0];
     var gestures = new Hammer(scenario1); // { prevent_default: true }
 
-    function setupRecognizers () {      
-        // Tap and Double Tap     
-        var singleTap = new Hammer.Tap({ event: EVENTS.tap, taps: 1 });
-        var doubleTap = new Hammer.Tap({ event: EVENTS.doubleTap, taps: 2 });
+    function playAudio () {
+        // enable Two Finger Tap to pause/resume
+        // disable zoom while audio is playing
 
-        gestures.add([doubleTap, singleTap]);
+        gestures.get(EVENTS.twoFingerTap).set({ enable: true });
+        gestures.get(EVENTS.pinch).set({ enable: false });
+    };
+    function stopAudio () {
+        // disable Two Finger Tap
+        // enable zoom
+
+        gestures.get(EVENTS.twoFingerTap).set({ enable: false });
+        gestures.get(EVENTS.pinch).set({ enable: true });
+    };
+
+    function setupRecognizers () { 
+        var singleTap = new Hammer.Tap();
+        var doubleTap = new Hammer.Tap({ event: EVENTS.doubleTap, taps: 2, interval: 800 });        
+        var twoFingerTap = new Hammer.Tap({ event: EVENTS.twoFingerTap, taps: 1, pointers: 2 });        
+        var zoom = new Hammer.Pinch({ event: EVENTS.pinch});
+        var hold = new Hammer.Press({ time: 500 });
         
         doubleTap.recognizeWith(singleTap);
         singleTap.requireFailure(doubleTap);        
 
-        // Tap and Hold
-        gestures.add(new Hammer.Press({ time: 500 }));     
-
-        // Zoom in/out
-        gestures.add(new Hammer.Pinch());
-        gestures.get(EVENTS.pinch).set({ enable: true });
-
-        // Swipe up/down (enable vertical swiping first)
-        gestures.add(new Hammer.Swipe());        
-        gestures.get(EVENTS.swipe).set({ direction: Hammer.DIRECTION_VERTICAL });
+        gestures.add([zoom, hold, twoFingerTap, doubleTap, singleTap]);
         
-    }
-
-    function setupEventsGeneral () {
-        gestures.on(EVENTS.doubleTap, function (e) {
-            alert('show menu');
-        });
-
-        // only when menu is visible
-        gestures.on('panup pandown', function (e) {
-            alert(e.type);
-        });
-    };
+        stopAudio();    
+    }  
 
     function setupEventsScene1 () {
+        $(document.body).on("contextmenu", function(e){
+            e.preventDefault();
+        });
+        
+        gestures.on(EVENTS.singleTap, function (e) {
+            console.log('play info audio (single tap)');
+        });
         gestures.on(EVENTS.doubleTap, function (e) {
             console.log('play info audio (double tap)- ' + e.tapCount);
         });
-        gestures.on(EVENTS.tap, function (e) {
-            console.log('play info audio (single tap) - ' + e.tapCount);
-        });
+        gestures.on(EVENTS.twoFingerTap, function (e) {            
+            console.log('play/pause audio (two finger tap)');                        
+        });       
+
         gestures.on(EVENTS.press, function (e) {
             console.log('get info surroundings (press)');
         });
-
-        gestures.on(EVENTS.pinchIn, function (e) {
-            console.log('zoom in');
-        });
-        gestures.on(EVENTS.pinchOut, function (e) {
-            console.log('zoom out');
-        });
-
-        gestures.on(EVENTS.swipe, function (e) {
-            console.log('move menu up - swipe up - ' + e);
-        });
-        // gestures.on(EVENTS.swipeDown, function (e) {
-        //     console.log('move menu down - swipe down');
-        // });
+        
+        gestures.on(EVENTS.pinchEnd, function (e) {
+            if (e.additionalEvent === EVENTS.pinchOut) {
+                console.log('pinch - zoom out');
+            } else if (e.additionalEvent === EVENTS.pinchIn) {
+                console.log('pinch - zoom in');
+            }            
+        });       
     }
     
 
     setupRecognizers();
-    //setupEventsGeneral();
     setupEventsScene1();
+
+    $(document).ready(function() {
+        $('.menu-slider').slick({
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 3,
+            arrows: false,
+            touchMove: true,
+            vertical: true,
+            verticalSwiping: true
+        });
+    });    
 }
 
 demo();
