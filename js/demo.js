c93f6ef96f8086bdff82bda9a309e04d1947de41
antonia <antonia.mab@gmail.com>
2020-11-22 12:43:05 +0000

    Added audio to instructions

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,7 @@
 function Demo () {    
     var eventsCtrl = new EventsController();
     var menuCtrl = new MenuController();
+    var audioCtrl = new AudioController();
     var onboardingCtrl = new Onboarding();
     var scenario1Ctrl = new Scenario1();
     var scenario2Ctrl = new Scenario2();
@@ -8,8 +9,8 @@ function Demo () {
     var scenes = {
         welcome: {
             elementId: 'welcome',
-            audioSrc: '',
-            length: 0
+            audioSrc: 'assets/audios/instructions/welcome.mp3',
+            length: 7000
         },
         onboarding: {
             elementId: 'onboarding',
@@ -18,8 +19,8 @@ function Demo () {
         },
         instructions1: {
             elementId: 'instructions1',
-            audioSrc: '',
-            length: 0
+            audioSrc: 'assets/audios/instructions/instructions1.mp3',
+            length: 14000
         },
         scenario1: {
             elementId: 'scenario1',
@@ -28,13 +29,13 @@ function Demo () {
         },
         instructions2: {
             elementId: 'instructions2',
-            audioSrc: '',
-            length: 0
+            audioSrc: 'assets/audios/instructions/instructions2.mp3',
+            length: 20000
         },
         instructions3: {
             elementId: 'instructions3',
-            audioSrc: '',
-            length: 0
+            audioSrc: 'assets/audios/instructions/instructions3.mp3',
+            length: 16000
         },
         scenario2: {
             elementId: 'scenario2',
@@ -43,13 +44,13 @@ function Demo () {
         },
         instructions4: {
             elementId: 'instructions4',
-            audioSrc: '',
-            length: 0
+            audioSrc: 'assets/audios/instructions/instructions4.mp3',
+            length: 19000
         },
         end: {
             elementId: 'endTest',
-            audioSrc: '',
-            length: 0
+            audioSrc: 'assets/audios/instructions/end.mp3',
+            length: 2000
         }
     };
 
@@ -58,6 +59,9 @@ function Demo () {
         $element.removeClass('hide');
 
         // play audio
+        if (audioSrc && audioSrc !== '') {
+            audioCtrl.play(audioSrc);
+        }
 
         // show skip option, if any
         var skipBtn = $(document).find('#' + elementId + ' + .skip');
@@ -84,21 +88,21 @@ function Demo () {
 
     // START TEST
     $(document).ready(function () {
-       showScene(scenes.welcome.elementId);
+       showScene(scenes.welcome.elementId, scenes.welcome.audioSrc);
     });
    
     // EXIT TEST
     var quitBtn = $('.quit');
     quitBtn.on('click', function () {
         hideAll();        
-        showScene(scenes.end.elementId);
+        showScene(scenes.end.elementId, scenes.end.audioSrc);
     });
 
     // START ONBOARDING
     var onboardingBtn = $('#onboardingStart');
     onboardingBtn.on('click', function () {
         hideScene(scenes.welcome.elementId);
-        showScene(scenes.onboarding.elementId);
+        showScene(scenes.onboarding.elementId, scenes.onboarding.audioSrc);
 
         onboardingCtrl.start();        
     });
@@ -114,7 +118,7 @@ function Demo () {
     var scenario1Btn =  $('#scenario1Start');
     scenario1Btn.on('click', function () {
         hideScene(scenes.instructions1.elementId);
-        showScene(scenes.scenario1.elementId);
+        showScene(scenes.scenario1.elementId, scenes.scenario1.audioSrc);
 
         scenario1Ctrl.start();        
     });
@@ -129,14 +133,14 @@ function Demo () {
     var instructions3Btn = $('#showInstructions3');
     instructions3Btn.on('click', function () {
         hideScene(scenes.instructions2.elementId);
-        showScene(scenes.instructions3.elementId);
+        showScene(scenes.instructions3.elementId, scenes.instructions3.audioSrc);
     });
 
     // START SCENARIO #2
     var scenario2Btn =  $('#scenario2Start');
     scenario2Btn.on('click', function () {
         hideScene(scenes.instructions3.elementId);
-        showScene(scenes.scenario2.elementId);
+        showScene(scenes.scenario2.elementId, scenes.scenario2.audioSrc);
  
         scenario2Ctrl.start();        
     });
@@ -150,15 +154,15 @@ function Demo () {
     // EVENTS - ON SCENES COMPLETED
     $(document).on(TCDEMO.ONBOARDING.finishedEvent, (data) => {
         hideScene(scenes.onboarding.elementId);
-        showScene(scenes.instructions1.elementId);
+        showScene(scenes.instructions1.elementId, scenes.instructions1.audioSrc);
     });
     $(document).on(TCDEMO.SCENARIO1.finishedEvent, (data) => {
         hideScene(scenes.scenario1.elementId);
-        showScene(scenes.instructions2.elementId);
+        showScene(scenes.instructions2.elementId, scenes.instructions2.audioSrc);
     });    
     $(document).on(TCDEMO.SCENARIO2.finishedEvent, (data) => {
         hideScene(scenes.scenario2.elementId);
-        showScene(scenes.instructions4.elementId);
+        showScene(scenes.instructions4.elementId, scenes.instructions4.audioSrc);
     });
 
     
