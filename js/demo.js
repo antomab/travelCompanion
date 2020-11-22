8d4e5437ec50ca540643f5d122d056bc035aa426
antonia <antonia.mab@gmail.com>
2020-11-22 13:06:06 +0000

    scale up menu

--- js/demo.js
+++ js/demo.js
@@ -166,82 +166,10 @@ function Demo () {
     });
 
     
-
-
-    // onboardingBtn.addClass('hide');
-    // var instructions1 = $('#instructions1');
-    // instructions1.removeClass('hide');
-    // read out instructions
-   
-
-    // var scenario1Btn = $('#scenario1Start');
-    // scenario1Btn.on('click', function () {
-    //     scenario1Btn.addClass('hide');
-    //     scenario1Ctrl.start();
-    // });
-
     // $(document).on(TCDEMO.MENU.itemChangedEvent, (data) => {
     //     console.log('item changed! ' + data.index);
     // });
 
-    // var badgeMenuInfo = {
-    //     selectorId: 'badgeMenuOnboarding',
-    //     chime: {
-    //         audioSrc: 'assets/audios/onboarding/beepShort.mp3',
-    //         length: 1000
-    //     },
-    //     onMenuClosed: {
-    //         audioSrc: 'assets/audios/onboarding/badgeDismissed.mp3',
-    //         length: 1000
-    //     }
-    // };
-    // var badgeMenuItems = [
-    //     {
-    //         index: 0,
-    //         description: 'first item',            
-    //         itemSelector: '.badge-1',
-    //         toSelector: '.to1',
-    //         slideOut: 'slide-out-158',
-    //         slideIn: 'slide-in',
-    //         audio: {                
-    //             onActive: {
-    //                 audioSrc: 'assets/audios/onboarding/badgeFirstItem.mp3',
-    //                 length: 100
-    //             },
-    //             onSelected: {
-    //                 audioSrc: 'assets/audios/onboarding/badgeFirstItemSelected.mp3',
-    //                 length: 1000
-    //             }
-    //         }
-    //     },
-    //     {
-    //         index: 1,
-    //         description: 'second item',
-    //         itemSelector: '.badge-2',
-    //         toSelector: '.to2',
-    //         slideOut: 'slide-out-6A',
-    //         slideIn: 'slide-in',
-    //         audio: {                
-    //             onActive: {
-    //                 audioSrc: 'assets/audios/onboarding/badgeSecondItem.mp3',
-    //                 length: 100,
-    //             },
-    //             onSelected: {
-    //                 audioSrc: 'assets/audios/onboarding/badgeSecondItemSelected.mp3',
-    //                 length: 1000,
-    //             }
-    //         }
-    //     }
-    // ];
-    // var badgeMenuCtrl = new BadgeMenuController(badgeMenuInfo, badgeMenuItems);
-    // var onboardingBtn = $('#onboardingStart');
-    // onboardingBtn.on('click', function () {
-    //     onboardingBtn.addClass('hide');
-        
-    //     var $onboardingElem = $('#onboarding');
-    //     $onboardingElem.removeClass('hide');
-    //     badgeMenuCtrl.show();      
-    // });
 }
 
 Demo();
